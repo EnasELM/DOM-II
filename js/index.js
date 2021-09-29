8f99148c753352094d075be46bc41e776e993525
Enas <enaselmahdwi@gmail.com>
2021-09-28 19:51:48 -0600

    update my work

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 // Your code goes here
 const btn=document.querySelector('.btn');
 btn.addEventListener('click',function() {
-    btn.innerHTML='BUTTON'
+    btn.innerHTML='BUTTON';
 });
 const nav=document.querySelector('.nav-link');
 nav.onmouseover=mouseOver;
 function mouseOver(){
-    nav.textContent='I AM HERE'
+    nav.textContent='I AM HERE';
     nav.style.color='green'
-}
+};
+const h1=document.querySelector('h1');
 nav.onmouseout=mouseOut;
 function mouseOut(){
     nav.textContent='I AM OUT'
@@ -21,16 +22,17 @@ p.addEventListener('focus',(event) =>{
     event.target.style.background = 'pink';
 });
 const inp=document.createElement('input');
-document.body.appendChild(inp);
+
+h1.insertAdjacentElement('afterend',inp);
 
 inp.addEventListener('focus',(event) =>{
     event.target.style.background = 'pink';
 });
-const h1=document.querySelector('h1')
+
 const h2=document.createElement('h2');
 
-h2.prepend('new');
-h2.insertAdjacentElement('afterend',h1);
+h2.innerHTML='I AM H2 JUST I ADD IT';
+h1.insertAdjacentElement('afterend',h2);
 
 const heightOutput = document.querySelector('h2');
 const widthOutput = document.querySelector('h2');
@@ -41,11 +43,28 @@ function Size() {
 window.onresize = Size;
 
 function myFunction() {
-var elmnt = document.querySelector('p');
-
+let elmnt = document.querySelector('p');
 elmnt.scrollLeft = 50;
-elmnt.scrollTop = 10;
+elmnt.scrollTop = 10}
 
-}
-btn.addEventListener('click',myFunction)
-  
+btn.addEventListener('click',myFunction())
+const select=document.createElement('select');
+const footer = document.querySelector('footer');
+select.style.backgroundColor='red';
+select.style.width='60px';
+select.item('select me');
+select.style.color='black'
+select.textContent='select'
+h1.insertAdjacentElement('beforeend',select);
+//dblclick
+btn.addEventListener('dblclick',function(){
+    h1.style.color='red';
+
+});
+document.addEventListener("drag", function(event) {
+
+}, false);
+document.addEventListener("drop", function(event) {
+    
+    event.preventDefault()
+})
